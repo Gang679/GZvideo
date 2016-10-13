94f2b9d3455c782bf7640bdf8c7b324a9e5d7bae
Gang679 <xinshijie@xinshijietekiimac.local>
2016-10-13 09:55:21 +0800

    Initial Commit

---
+++ video/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  video
+//
+//  Created by xinshijie on 16/10/13.
+//  Copyright © 2016年 Mr.quan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
